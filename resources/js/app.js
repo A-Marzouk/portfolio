d4737fcef91cda7b5acf15f556496d9d1b45d707
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-01-31 01:21:09 +0200

    Add cool theme to the portfolio

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
+import $ from "jquery";
 require('./bootstrap');
+
+window.Vue = require('vue');
+import VueParticles from 'vue-particles';
+Vue.use(VueParticles);
+
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
+import theme from './components/theme8';
+
+new Vue({
+    el: '#theme',
+    components:{
+        'my-theme':theme
+    }
+});
